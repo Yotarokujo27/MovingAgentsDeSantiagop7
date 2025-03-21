c7418a866672b945adb7d48b5782a3ecfda09f97
Alfredo De Santiago <727601@student.dusd.net>
2025-03-21 15:25:08 -0700

    did some
    did some stuff on the project

--- DeSantiagpP7MovingAgents/Assets/Bot.cs
+++ DeSantiagpP7MovingAgents/Assets/Bot.cs
@@ -8,10 +8,13 @@ public class Bot : MonoBehaviour
 
     NavMeshAgent agent;
     public GameObject target;
+    Drive ds;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
+        ds = target.GetComponent<Drive>();
     }
 
     void Seek(Vector3 location)
@@ -25,9 +28,30 @@ public class Bot : MonoBehaviour
         agent.SetDestination(this.transform.position - fleeVector);
     }
 
+    void Pursue()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
+        if ((toTarget > 90 && relativeHeading < 20) || ds.currentSpeed < 0.01f)
+        {
+            Seek(target.transform.position);
+        }
+
+        float lookAhead = targetDir.magnitude/(agent.speed + ds.currentSpeed);
+        Seek(target.transform.position + target.transform.forward * lookAhead);
+    }
+
+    void Evade()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
+        Flee(target.transform.position + target.transform.forward * lookAhead);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Flee(target.transform.position);
+        Evade();
     }
 }
